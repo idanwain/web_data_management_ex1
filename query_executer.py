0f571c331f6d1ab8f752a6b8086b294f6f18a890
idanwain <idanwaintraub11@gmail.com>
2021-05-13 17:08:18 +0300

    patterns

--- query_executer.py
+++ query_executer.py
@@ -1,3 +1,25 @@
+import re
+import sys
+
+patterns = [
+    'who directed [^\s].*[^\s]\?',
+    'who produced [^\s].*[^\s]\?',
+    'is [^\s].*[^\s] based on a book\?',
+    'when was [^\s].*[^\s] released\?',
+    'how long is [^\s].*[^\s]\?',
+    'who starred in [^\s].*[^\s]\?',
+    'did [^\s].*[^\s] star in [^\s].*[^\s]\?',
+    'when was [^\s].*[^\s] born\?',
+    'what is the occupation of [^\s].*[^\s]\?',
+    'how many films are based on books\?',
+    'how many films starring [^\s].*[^\s] won an academy award\?',
+    'how many [^\s].*[^\s] are also [^\s].*[^\s]\?',
+]
+
 
 def execute(query):
+    matching_pattern = get_matching_pattern(query)
+    if not matching_pattern:
+        print('please enter a valid query.')
+        sys.exit(0)
     return None

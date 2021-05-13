b7a75e018e581d664b07ebd2c247f057aa4e09b5
roye <tadmoroye@gmail.com>
2021-05-13 17:30:21 +0300

    added extract entity from query

--- query_executer.py
+++ query_executer.py
@@ -2,28 +2,34 @@ import re
 import sys
 
 patterns = [
-    'who directed [^\s].*[^\s]\?',
-    'who produced [^\s].*[^\s]\?',
-    'is [^\s].*[^\s] based on a book\?',
-    'when was [^\s].*[^\s] released\?',
-    'how long is [^\s].*[^\s]\?',
-    'who starred in [^\s].*[^\s]\?',
-    'did [^\s].*[^\s] star in [^\s].*[^\s]\?',
-    'when was [^\s].*[^\s] born\?',
-    'what is the occupation of [^\s].*[^\s]\?',
+    'who directed ([^\s].*[^\s])\?',
+    'who produced ([^\s].*[^\s])\?',
+    'is ([^\s].*[^\s]) based on a book\?',
+    'when was ([^\s].*[^\s]) released\?',
+    'how long is ([^\s].*[^\s])\?',
+    'who starred in ([^\s].*[^\s])\?',
+    'did ([^\s].*[^\s]) star in ([^\s].*[^\s])\?',
+    'when was ([^\s].*[^\s]) born\?',
+    'what is the occupation of ([^\s].*[^\s])\?',
     'how many films are based on books\?',
-    'how many films starring [^\s].*[^\s] won an academy award\?',
-    'how many [^\s].*[^\s] are also [^\s].*[^\s]\?',
+    'how many films starring ([^\s].*[^\s]) won an academy award\?',
+    'how many ([^\s].*[^\s]) are also ([^\s].*[^\s])\?',
 ]
 
 
 def get_matching_pattern(query):
     pass
 
+def get_entities_from_query(pattern,query):
+    p = re.compile(pattern)
+    res = p.findall(query)
+    lst = list(res[0])
+    return lst
 
 def execute(query):
     matching_pattern = get_matching_pattern(query)
     if not matching_pattern:
         print('please enter a valid query.')
         sys.exit(0)
+
     return None
